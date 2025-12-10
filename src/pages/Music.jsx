5a2d59b15b6a36d64d344d98cb9764e4a4b93d5b
Paul Pommer <paul.pommer@ki-macht-schule.de>
2025-12-10 15:59:30 +0100

    Update music section

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -3,7 +3,7 @@ function Music() {
     <div className="px-4 md:px-14 lg:px-28 overflow-hidden">
       <div className="lg:w-[70%] mb-20">
         <h1 className="text-5xl font-bold mb-8 text-zinc-800 dark:text-zinc-100">
-          DJing
+          Pommerotti
         </h1>
         <p className="text-zinc-600 dark:text-zinc-400">
           A collection of mixtapes and livesets. Enjoy!
