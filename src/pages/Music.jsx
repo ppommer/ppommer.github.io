0c06788de42ddb87189bab5bccb6f768d0b5f7ee
Paul Pommer <paul.pommer@ki-macht-schule.de>
2025-12-10 16:59:29 +0100

    Update Music tab

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,7 +1,7 @@
 function Music() {
   return (
     <div className="px-4 md:px-14 lg:px-28 overflow-hidden">
-      <div className="lg:w-[70%] mb-20">
+      <div className="lg:w-[70%] mb-10">
         <h1 className="text-5xl font-bold mb-8 text-zinc-800 dark:text-zinc-100">
           Pommerotti
         </h1>
@@ -18,38 +18,6 @@ function Music() {
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/soundcloud%3Aplaylists%3A1668063167%3Fsecret_token%3Ds-zqYfeT4OflK&color=%235bdefa&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             title="SoundCloud Playlist"
           ></iframe>
-          <div
-            style={{
-              fontSize: "10px",
-              color: "#cccccc",
-              lineBreak: "anywhere",
-              wordBreak: "normal",
-              overflow: "hidden",
-              whiteSpace: "nowrap",
-              textOverflow: "ellipsis",
-              fontWeight: 100,
-            }}
-          >
-            <a
-              href="https://soundcloud.com/pommerotti"
-              title="Pommerotti"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: "#cccccc", textDecoration: "none" }}
-            >
-              Pommerotti
-            </a>{" "}
-            ·{" "}
-            <a
-              href="https://soundcloud.com/pommerotti/sets/favorites/s-zqYfeT4OflK"
-              title="Favorites"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: "#cccccc", textDecoration: "none" }}
-            >
-              Favorites
-            </a>
-          </div>
         </div>
       </div>
     </div>
